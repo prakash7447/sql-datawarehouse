24daf7a7c32324fc87499575bcdf1a752c67605c
prakash7447 <129209006+prakash7447@users.noreply.github.com>
2026-02-20 12:43:42 +0530

    Update fix.sql

--- scripts/silver/fix.sql
+++ scripts/silver/fix.sql
@@ -27,3 +27,30 @@ where (cust_rank_by_date = 1 and cst_id is not null);
 
 
 select * from silver.crm_cust_info
+
+--inserting into crm_prd_info
+insert into silver.crm_prd_info
+(prd_id,
+ cat_id,
+ prd_key,
+ prd_nm,          
+ prd_cost,
+ prd_line,
+ prd_start_dt,
+ prd_end_dt )
+ (
+select prd_id,
+replace(SUBSTRING(prd_key,1,5),'-','_') as cat_id,
+SUBSTRING(prd_key,7,len(prd_key)-1) as prd_key,
+prd_nm,
+isnull(prd_cost,0) as prd_cost,
+case 
+when upper(trim(prd_line)) ='M' then 'Mountain'
+when upper(trim(prd_line)) ='R' then 'Road'
+when upper(trim(prd_line)) ='S' then 'Other sales'
+when upper(trim(prd_line)) ='T' then 'Touring'
+else 'n/a'
+end as prd_line,
+cast(prd_start_dt as date) as prd_start_dt,
+cast(lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)-1 as date ) as prd_end_dt
+from bronze.crm_prd_info);
